8b174dad3a9ffbd3dc95aeb62f030d2839f714e9
parniSDU <66359082+parniSDU@users.noreply.github.com>
2022-03-03 12:10:19 +0100

    Update main.R

--- R/main.R
+++ R/main.R
@@ -98,19 +98,59 @@ TM<-function(G, dist,param, t, delta, states,...){
 }
 
 
-
-#Calculate Age Intensity
+#' Age Intensity Function
+#'
+#' This function returns a numeric value indicating
+#' the pending time (in terms of time steps, delta) for a given state change of a proxel.
+#'
+#' @param state a string value
+#' @param proxel a data frame containing a state, age intensity and the probability.
+#'
+#' @details The more details should be added.
+#'
+#' @examples
+#' proxel<-data.frame(State="OK",ageInt=0, Prob=1)
+#' state<-"OK"
+#' calageInt(state, proxel)
+#' @export
 calageInt<-function(state, proxel){
   ifelse (proxel$State!=state, ageint<-0,ageint<-proxel$ageInt+delta)
   ageint
 }
 
-# Calculate Probability
-calProb<-function(BE, s,proxel,delta){
+
+#' Transition Probability Function
+#'
+#' This function returns the transition probability for a given basic event and a proxel.
+#'
+#' @param BE a list containing states, transition matrix, distributions and their parameters for a basic event
+#' @param state a string value
+#' @param proxel a data frame containing a state, age intensity and a probability.
+#' @param delta a numeric value as time step
+#'
+#' @details The more details should be added.
+#'
+#' @examples
+#' ## A repairable basic event with Uniform(2, 2.5) failure distribution function
+#' ## and a fixed repair time of 0.3.
+#' BE<-list(
+#'states=c("OK","F"),
+#'G=rbind(c(NA,1),
+#'        c(1,NA)),
+#'dist=c("unif", "unif"),
+#'param=list(c(2, 2.5), c(0.3-delta,0.3+delta))
+#')
+#'state<-"OK"
+#'proxel<-data.frame(State="OK",ageInt=0, Prob=1)
+#'delta<-0.2
+#'calProb(BE, state,proxel,delta)
+#' @export
+calProb<-function(BE, state, proxel, delta){
   t<-proxel$ageInt
   m<-TM(BE$G, BE$dist, BE$param, t,delta, BE$states)
-  m[proxel$State,s]
+  m[proxel$State,state]
 }
+
 
 
 #Next Level
