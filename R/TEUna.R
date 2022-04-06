62ae2255972ac96a8df6977e6318cde44a9d6088
haghbinh <haghbinh@gmail.com>
2022-04-06 18:39:18 +0430

    TEUna function

--- R/TEUna.R
+++ R/TEUna.R
@@ -1,5 +1,19 @@
+#' The title of function
+#'
+#' This function returns a ....
+#'
+#' @param mcs a ...
+#' @param una a ...
+#'
+#' @return  A numeric matrix of transition probabilities.
+#'
+#' @details The more details should be added.
+#'
+#' @examples
+#' ...
+#'
 #' @export
-TEUna<-function(mcs,una){
+TEUna <- function(mcs,una){
   te<-0
   for (i in 1:length(mcs)){
     if (length(mcs[[i]])==1){p<-una[,mcs[[i]]]}else{
